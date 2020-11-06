5072d427614144c746cc51f33ea561bca31fa8ef
tonyng <tonyng987@gmail.com>
2020-11-05 20:37:40 -0800

    Refactor post_index_item using hooks

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -1778,153 +1778,114 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _like_like_container__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../like/like_container */ "./frontend/components/like/like_container.js");
 /* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 
 
 
 
-var PostIndexItem =
-/*#__PURE__*/
-function (_React$Component) {
-  _inherits(PostIndexItem, _React$Component);
+function PostIndexItem(props) {
+  var _useState = Object(react__WEBPACK_IMPORTED_MODULE_0__["useState"])(""),
+      _useState2 = _slicedToArray(_useState, 2),
+      body = _useState2[0],
+      setBody = _useState2[1];
 
-  function PostIndexItem(props) {
-    var _this;
+  function handleComment(e) {
+    e.preventDefault();
 
-    _classCallCheck(this, PostIndexItem);
-
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(PostIndexItem).call(this, props));
-    _this.state = {
-      body: ""
-    };
-    _this.handleComment = _this.handleComment.bind(_assertThisInitialized(_this));
-    return _this;
+    if (body !== "") {
+      var comment = {
+        body: body,
+        post_id: props.post.id
+      };
+      props.createComment(comment);
+      props.fetchPost(props.post.id);
+      setBody("");
+    } else {
+      props.openErrorModal();
+    }
   }
 
-  _createClass(PostIndexItem, [{
-    key: "handleComment",
-    value: function handleComment(e) {
-      e.preventDefault();
+  function update(field) {
+    if (field === "body") return function (e) {
+      return setBody(e.target.value);
+    };
+  }
 
-      if (this.state.body !== "") {
-        var comment = {
-          body: this.state.body,
-          post_id: this.props.post.id
-        };
-        this.props.createComment(comment);
-        this.props.fetchPost(this.props.post.id);
-        this.setState({
-          body: ""
-        });
-      } else {
-        this.props.openErrorModal();
-      }
-    }
-  }, {
-    key: "update",
-    value: function update(field) {
-      var _this2 = this;
+  var post = props.post;
+  var commentPosts = Object.values(post.comments).map(function (comment) {
+    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      key: comment.id,
+      className: "post-show-comment"
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__["Link"], {
+      className: "feed-profile-link",
+      to: "/users/".concat(comment.user_id)
+    }, comment.author), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+      className: "comment-bo"
+    }, "\xA0", comment.body));
+  });
+  var limitPosts = [];
 
-      return function (e) {
-        return _this2.setState(_defineProperty({}, field, e.target.value));
-      };
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      var _this3 = this;
+  while (limitPosts.length < 3) {
+    limitPosts.push(commentPosts.shift());
+  }
 
-      var post = this.props.post;
-      var commentPosts = Object.values(post.comments).map(function (comment) {
-        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-          key: comment.id,
-          className: "post-show-comment"
-        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__["Link"], {
-          className: "feed-profile-link",
-          to: "/users/".concat(comment.user_id)
-        }, comment.author), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
-          className: "comment-bo"
-        }, "\xA0", comment.body));
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    className: "feed-image-box"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "feed-image-header"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "feed-image-header-wrap"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    className: "feed-profile-pic",
+    src: post.authorPhotoUrl
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__["Link"], {
+    className: "profile-link",
+    to: "/users/".concat(post.user_id)
+  }, post.author)), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    onClick: function onClick() {
+      return props.openModal({
+        postId: post.id
       });
-      var limitPosts = [];
-
-      while (limitPosts.length < 3) {
-        limitPosts.push(commentPosts.shift());
-      }
-
-      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
-        className: "feed-image-box"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "feed-image-header"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "feed-image-header-wrap"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-        className: "feed-profile-pic",
-        src: post.authorPhotoUrl
-      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__["Link"], {
-        className: "profile-link",
-        to: "/users/".concat(post.user_id)
-      }, post.author)), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        onClick: function onClick() {
-          return _this3.props.openModal({
-            postId: post.id
-          });
-        }
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-        className: "feed-image",
-        src: post.photoUrl
-      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "title-span"
-      }, post.title)), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "feed-image-bottom"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "feed-image-bottom-likes"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_like_like_container__WEBPACK_IMPORTED_MODULE_1__["default"], {
-        post: post,
-        likers: post.likers
-      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "likes"
-      }, post.likers.length === 1 ? "1 like" : "".concat(post.likers.length, " likes"))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "feed-image-bottom-bio"
-      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "feed-image-bottom-comments"
-      }, limitPosts), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "feed-image-comment-input"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("form", {
-        className: "feed-comment-form"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
-        className: "feed-textarea",
-        value: this.state.body,
-        onChange: this.update("body"),
-        placeholder: "Add a comment..."
-      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
-        className: "submit-comment-button-index",
-        onClick: this.handleComment
-      }, "Post"))))));
     }
-  }]);
-
-  return PostIndexItem;
-}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    className: "feed-image",
+    src: post.photoUrl
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "title-span"
+  }, post.title)), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "feed-image-bottom"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "feed-image-bottom-likes"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_like_like_container__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    post: post,
+    likers: post.likers
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "likes"
+  }, post.likers.length === 1 ? "1 like" : "".concat(post.likers.length, " likes"))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "feed-image-bottom-bio"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "feed-image-bottom-comments"
+  }, limitPosts), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "feed-image-comment-input"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("form", {
+    className: "feed-comment-form"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+    className: "feed-textarea",
+    value: body,
+    onChange: update("body"),
+    placeholder: "Add a comment..."
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+    className: "submit-comment-button-index",
+    onClick: handleComment
+  }, "Post"))))));
+}
 
 /* harmony default export */ __webpack_exports__["default"] = (PostIndexItem);
 
--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -1,98 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import LikeContainer from "../like/like_container";
 import { Link } from "react-router-dom";
 
-class PostIndexItem extends React.Component {
-  constructor(props) {
-    super(props);
+function PostIndexItem(props) {
+  const [body, setBody] = useState("");
 
-    this.state = { body: "" };
-
-    this.handleComment = this.handleComment.bind(this);
-  }
-
-  handleComment(e) {
+  function handleComment(e) {
     e.preventDefault();
-    if (this.state.body !== "") {
-      const comment = { body: this.state.body, post_id: this.props.post.id };
-      this.props.createComment(comment);
-      this.props.fetchPost(this.props.post.id);
-      this.setState({ body: "" });
+    if (body !== "") {
+      const comment = { body: body, post_id: props.post.id };
+      props.createComment(comment);
+      props.fetchPost(props.post.id);
+      setBody("");
     } else {
-      this.props.openErrorModal();
+      props.openErrorModal();
     }
   }
 
-  update(field) {
-    return e => this.setState({ [field]: e.target.value });
+  function update(field) {
+    if (field === "body") return e => setBody(e.target.value);
   }
 
-  render() {
-    let { post } = this.props;
-    
-    let commentPosts = Object.values(post.comments).map(comment => {
-      return (
-        <div key={comment.id} className="post-show-comment">
-          <Link className="feed-profile-link" to={`/users/${comment.user_id}`}>
-            {comment.author}
-          </Link>
-          <span className="comment-bo">&nbsp;{comment.body}</span>
-        </div>
-      );
-    });
-    let limitPosts = [];
-    while (limitPosts.length < 3) {
-      limitPosts.push(commentPosts.shift());
-    }
 
-    return(
-        <li className = "feed-image-box" >
-        <div className="feed-image-header">
-          <div className="feed-image-header-wrap">
-            <img className="feed-profile-pic" src={post.authorPhotoUrl} />
-            <Link className="profile-link" to={`/users/${post.user_id}`}>
-              {post.author}
-            </Link>
-          </div>
-          <div onClick={() => this.props.openModal({ postId: post.id })}>
-            <img className="feed-image" src={post.photoUrl} />
-            <div className="title-span">{post.title}</div>
-          </div>
-          <div className="feed-image-bottom">
-            <div className="feed-image-bottom-likes">
-              <LikeContainer post={post} likers={post.likers} />
-              <div className="likes">
-                {post.likers.length === 1
-                  ? `1 like`
-                  : `${post.likers.length} likes`}
-              </div>
-            </div>
-            <div className="feed-image-bottom-bio"></div>
-            <div className="feed-image-bottom-comments">{limitPosts}</div>
-            <div className="feed-image-comment-input">
-              <form className="feed-comment-form">
-                <input
-                  className="feed-textarea"
-                  value={this.state.body}
-                  onChange={this.update("body")}
-                  placeholder="Add a comment..."
-                ></input>
-                <button
-                  className="submit-comment-button-index"
-                  onClick={this.handleComment}
-                >
-                  Post
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-        </li>
+  let { post } = props;
+  let commentPosts = Object.values(post.comments).map(comment => {
+    return (
+      <div key={comment.id} className="post-show-comment">
+        <Link className="feed-profile-link" to={`/users/${comment.user_id}`}>
+          {comment.author}
+        </Link>
+        <span className="comment-bo">&nbsp;{comment.body}</span>
+      </div>
     );
+  });
+  let limitPosts = [];
+  while (limitPosts.length < 3) {
+    limitPosts.push(commentPosts.shift());
   }
 
+  return(
+      <li className = "feed-image-box" >
+      <div className="feed-image-header">
+        <div className="feed-image-header-wrap">
+          <img className="feed-profile-pic" src={post.authorPhotoUrl} />
+          <Link className="profile-link" to={`/users/${post.user_id}`}>
+            {post.author}
+          </Link>
+        </div>
+        <div onClick={() => props.openModal({ postId: post.id })}>
+          <img className="feed-image" src={post.photoUrl} />
+          <div className="title-span">{post.title}</div>
+        </div>
+        <div className="feed-image-bottom">
+          <div className="feed-image-bottom-likes">
+            <LikeContainer post={post} likers={post.likers} />
+            <div className="likes">
+              {post.likers.length === 1
+                ? `1 like`
+                : `${post.likers.length} likes`}
+            </div>
+          </div>
+          <div className="feed-image-bottom-bio"></div>
+          <div className="feed-image-bottom-comments">{limitPosts}</div>
+          <div className="feed-image-comment-input">
+            <form className="feed-comment-form">
+              <input
+                className="feed-textarea"
+                value={body}
+                onChange={update("body")}
+                placeholder="Add a comment..."
+              ></input>
+              <button
+                className="submit-comment-button-index"
+                onClick={handleComment}
+              >
+                Post
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+      </li>
+  );
 }
 
 export default PostIndexItem
+
+  
 
   
