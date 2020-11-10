2e43424f965f0177f0696a3d955888be92ead31c
tonyng <tonyng987@gmail.com>
2020-11-09 20:20:52 -0800

    Refactor search_bar using hooks

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -1200,116 +1200,168 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+// import React from "react";
+// import { Link, withRouter } from "react-router-dom";
+// class SearchBar extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       searchField: "",
+//       matches: []
+//     };
+//   }
+//   componentDidMount() {
+//     this.props.fetchUsers();
+//   }
+//   componentDidUpdate(prevProps) {
+//     if (prevProps.location.pathname !== this.props.location.pathname) {
+//       this.setState({
+//         searchField: "",
+//         matches: []
+//       });
+//     }
+//   }
+//   update() {
+//     return e => {
+//       let userMatches = this.props.users.filter(user =>
+//         user.username.toUpperCase().includes(e.target.value.toUpperCase())
+//       );
+//       if (!e.target.value) userMatches = [];
+//       this.setState({
+//         matches: userMatches,
+//         searchField: e.target.value
+//       });
+//     };
+//   }
+//   render() {
+//     return (
+//       <div className="user-search-results">
+//         <input
+//           className="user-search"
+//           type="text"
+//           placeholder="Search Users"
+//           onChange={this.update()}
+//           value={this.state.searchField}
+//         />
+//         <div className="outer-results">
+//           {this.state.matches.length ? (
+//             <div className="search-results-container">
+//               <div id="arrow"></div>
+//               <ul className="search-results-list">
+//                 {this.state.matches.map(user => {
+//                   return (
+//                     <li className="search-results-li" key={user.id}>
+//                       <Link
+//                         className="search-result-user"
+//                         to={`/users/${user.id}`}
+//                       >
+//                         <img
+//                           className="search-result-image"
+//                           src={user.photo_url}
+//                         />
+//                         <div className="search-result-username">
+//                           {user.username}
+//                         </div>
+//                       </Link>
+//                     </li>
+//                   );
+//                 })}
+//               </ul>
+//             </div>
+//           ) : (
+//               <div></div>
+//             )}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// export default withRouter(SearchBar);
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 
+function SearchBar(props) {
+  var _useState = Object(react__WEBPACK_IMPORTED_MODULE_0__["useState"])(''),
+      _useState2 = _slicedToArray(_useState, 2),
+      searchField = _useState2[0],
+      setSearchField = _useState2[1];
 
+  var _useState3 = Object(react__WEBPACK_IMPORTED_MODULE_0__["useState"])([]),
+      _useState4 = _slicedToArray(_useState3, 2),
+      matches = _useState4[0],
+      setMatches = _useState4[1];
 
-var SearchBar =
-/*#__PURE__*/
-function (_React$Component) {
-  _inherits(SearchBar, _React$Component);
-
-  function SearchBar(props) {
-    var _this;
-
-    _classCallCheck(this, SearchBar);
-
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(SearchBar).call(this, props));
-    _this.state = {
-      searchField: "",
-      matches: []
-    };
-    return _this;
-  }
-
-  _createClass(SearchBar, [{
-    key: "componentDidMount",
-    value: function componentDidMount() {
-      this.props.fetchUsers();
-    }
-  }, {
-    key: "componentDidUpdate",
-    value: function componentDidUpdate(prevProps) {
-      if (prevProps.location.pathname !== this.props.location.pathname) {
-        this.setState({
-          searchField: "",
-          matches: []
-        });
+  var mounted = Object(react__WEBPACK_IMPORTED_MODULE_0__["useRef"])();
+  Object(react__WEBPACK_IMPORTED_MODULE_0__["useEffect"])(function (prevProps) {
+    if (!mounted.current) {
+      props.fetchUsers();
+      mounted.current = true;
+    } else {
+      if (prevProps.location.pathname !== props.location.pathname) {
+        setSearchField('');
+        setMathces([]);
       }
     }
-  }, {
-    key: "update",
-    value: function update() {
-      var _this2 = this;
+  }); // useEffect(() => {
+  //   props.fetchUsers();
+  // }, [])
+  // useEffect((prevProps) => {
+  //   if (prevProps.location.pathname !== props.location.pathname) {
+  //     setSearchField('');
+  //     setMathces([]);
+  //   }
+  // }, [])
 
-      return function (e) {
-        var userMatches = _this2.props.users.filter(function (user) {
-          return user.username.toUpperCase().includes(e.target.value.toUpperCase());
-        });
+  function update() {
+    return function (e) {
+      var userMatches = props.users.filter(function (user) {
+        return user.username.toUpperCase().includes(e.target.value.toUpperCase());
+      });
+      if (!e.target.value) userMatches = [];
+      setMatches(userMatches);
+      setSearchField(e.target.value);
+    };
+  }
 
-        if (!e.target.value) userMatches = [];
-
-        _this2.setState({
-          matches: userMatches,
-          searchField: e.target.value
-        });
-      };
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "user-search-results"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
-        className: "user-search",
-        type: "text",
-        placeholder: "Search Users",
-        onChange: this.update(),
-        value: this.state.searchField
-      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "outer-results"
-      }, this.state.matches.length ? react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "search-results-container"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        id: "arrow"
-      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {
-        className: "search-results-list"
-      }, this.state.matches.map(function (user) {
-        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
-          className: "search-results-li",
-          key: user.id
-        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
-          className: "search-result-user",
-          to: "/users/".concat(user.id)
-        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-          className: "search-result-image",
-          src: user.photo_url
-        }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-          className: "search-result-username"
-        }, user.username)));
-      }))) : react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null)));
-    }
-  }]);
-
-  return SearchBar;
-}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "user-search-results"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+    className: "user-search",
+    type: "text",
+    placeholder: "Search Users",
+    onChange: update(),
+    value: searchField
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "outer-results"
+  }, matches.length ? react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "search-results-container"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    id: "arrow"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {
+    className: "search-results-list"
+  }, matches.map(function (user) {
+    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+      className: "search-results-li",
+      key: user.id
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
+      className: "search-result-user",
+      to: "/users/".concat(user.id)
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+      className: "search-result-image",
+      src: user.photo_url
+    }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "search-result-username"
+    }, user.username)));
+  }))) : react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null)));
+}
 
 /* harmony default export */ __webpack_exports__["default"] = (Object(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["withRouter"])(SearchBar));
 
--- frontend/components/nav_bar/search_bar.jsx
+++ frontend/components/nav_bar/search_bar.jsx
@@ -1,83 +1,169 @@
-import React from "react";
+// import React from "react";
+// import { Link, withRouter } from "react-router-dom";
+
+// class SearchBar extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       searchField: "",
+//       matches: []
+//     };
+//   }
+
+//   componentDidMount() {
+//     this.props.fetchUsers();
+//   }
+
+//   componentDidUpdate(prevProps) {
+//     if (prevProps.location.pathname !== this.props.location.pathname) {
+//       this.setState({
+//         searchField: "",
+//         matches: []
+//       });
+//     }
+//   }
+
+//   update() {
+//     return e => {
+//       let userMatches = this.props.users.filter(user =>
+//         user.username.toUpperCase().includes(e.target.value.toUpperCase())
+//       );
+//       if (!e.target.value) userMatches = [];
+//       this.setState({
+//         matches: userMatches,
+//         searchField: e.target.value
+//       });
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div className="user-search-results">
+//         <input
+//           className="user-search"
+//           type="text"
+//           placeholder="Search Users"
+//           onChange={this.update()}
+//           value={this.state.searchField}
+//         />
+//         <div className="outer-results">
+//           {this.state.matches.length ? (
+//             <div className="search-results-container">
+//               <div id="arrow"></div>
+//               <ul className="search-results-list">
+//                 {this.state.matches.map(user => {
+//                   return (
+//                     <li className="search-results-li" key={user.id}>
+//                       <Link
+//                         className="search-result-user"
+//                         to={`/users/${user.id}`}
+//                       >
+//                         <img
+//                           className="search-result-image"
+//                           src={user.photo_url}
+//                         />
+//                         <div className="search-result-username">
+//                           {user.username}
+//                         </div>
+//                       </Link>
+//                     </li>
+//                   );
+//                 })}
+//               </ul>
+//             </div>
+//           ) : (
+//               <div></div>
+//             )}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default withRouter(SearchBar);
+
+import React, { useState, useEffect, useRef } from "react";
 import { Link, withRouter } from "react-router-dom";
 
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchField: "",
-      matches: []
-    };
-  }
+function SearchBar(props) {
+  const [searchField, setSearchField] = useState('');
+  const [matches, setMatches] = useState([]);
+  const mounted = useRef();
 
-  componentDidMount() {
-    this.props.fetchUsers();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.location.pathname !== this.props.location.pathname) {
-      this.setState({
-        searchField: "",
-        matches: []
-      });
+  useEffect((prevProps) => {
+    if (!mounted.current) {
+      props.fetchUsers();
+      mounted.current = true;
+    } else {
+      if (prevProps.location.pathname !== props.location.pathname) {
+        setSearchField('');
+        setMathces([]);
+      }
     }
-  }
+  });
 
-  update() {
+  // useEffect(() => {
+  //   props.fetchUsers();
+  // }, [])
+
+  // useEffect((prevProps) => {
+  //   if (prevProps.location.pathname !== props.location.pathname) {
+  //     setSearchField('');
+  //     setMathces([]);
+  //   }
+  // }, [])
+
+  function update() {
     return e => {
-      let userMatches = this.props.users.filter(user =>
+      let userMatches = props.users.filter(user =>
         user.username.toUpperCase().includes(e.target.value.toUpperCase())
       );
       if (!e.target.value) userMatches = [];
-      this.setState({
-        matches: userMatches,
-        searchField: e.target.value
-      });
-    };
+      setMatches(userMatches);
+      setSearchField(e.target.value);
+    }
   }
 
-  render() {
-    return (
-      <div className="user-search-results">
-        <input
-          className="user-search"
-          type="text"
-          placeholder="Search Users"
-          onChange={this.update()}
-          value={this.state.searchField}
-        />
-        <div className="outer-results">
-          {this.state.matches.length ? (
-            <div className="search-results-container">
-              <div id="arrow"></div>
-              <ul className="search-results-list">
-                {this.state.matches.map(user => {
-                  return (
-                    <li className="search-results-li" key={user.id}>
-                      <Link
-                        className="search-result-user"
-                        to={`/users/${user.id}`}
-                      >
-                        <img
-                          className="search-result-image"
-                          src={user.photo_url}
-                        />
-                        <div className="search-result-username">
-                          {user.username}
-                        </div>
-                      </Link>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          ) : (
-              <div></div>
-            )}
-        </div>
+  return (
+    <div className="user-search-results">
+      <input
+        className="user-search"
+        type="text"
+        placeholder="Search Users"
+        onChange={update()}
+        value={searchField}
+      />
+      <div className="outer-results">
+        {matches.length ? (
+          <div className="search-results-container">
+            <div id="arrow"></div>
+            <ul className="search-results-list">
+              {matches.map(user => {
+                return (
+                  <li className="search-results-li" key={user.id}>
+                    <Link
+                      className="search-result-user"
+                      to={`/users/${user.id}`}
+                    >
+                      <img
+                        className="search-result-image"
+                        src={user.photo_url}
+                      />
+                      <div className="search-result-username">
+                        {user.username}
+                      </div>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        ) : (
+            <div></div>
+          )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default withRouter(SearchBar);
